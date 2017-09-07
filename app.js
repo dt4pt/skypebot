abe0f3e58381e89c52349f80fcf82b8aa55cebce
dt4pt <31660569+dt4pt@users.noreply.github.com>
2017-09-07 09:30:30 +0100

    Test
    Test to see if github is synching to a branch.

--- app.js
+++ app.js
@@ -20,3 +20,5 @@ server.post('/api/messages', connector.listen());
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
 });
+
+//have I updated the file?
